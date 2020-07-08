bda3ffccf7b4d182daf6f4a60f7b6e068eb34cc3
Wim Haeck <whaeck@gmail.com>
2020-07-08 07:47:03 -0600

    Updated the particle identifier to use a string

--- src/elementary/ParticleID/src/ctor.hpp
+++ src/elementary/ParticleID/src/ctor.hpp
@@ -6,3 +6,14 @@
  */
 ParticleID( IdentifierVariant&& identifier ) :
   identifier_( std::move( identifier ) ) {}
+
+/**
+ *  @brief Constructor
+ *
+ *  This function throws an invalid_argument exception if invalid data is
+ *  used.
+ *
+ *  @param[in] string   the string representation of the particle
+ */
+ParticleID( const std::string& string ) :
+  identifier_( matchIdentifier( string ) ) {}
