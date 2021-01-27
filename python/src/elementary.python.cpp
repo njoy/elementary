fff4fb7e31bb5d36be17301c9d0b4cacc3b0da13
Wim Haeck <whaeck@gmail.com>
2021-01-27 14:57:34 -0700

    Set up cmake for python bindings

---
+++ python/src/elementary.python.cpp
@@ -0,0 +1,47 @@
+// system includes
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+// namespace aliases
+namespace python = pybind11;
+
+// declarations
+// void wrapElementID( python::module& );
+// void wrapFundamentalParticleID( python::module& );
+// void wrapIsotopeID( python::module& );
+// void wrapLevel( python::module& );
+// void wrapNucleusID( python::module& );
+// void wrapNuclideID( python::module& );
+// void wrapParticleID( python::module& );
+// void wrapParticlePairID( python::module& );
+// void wrapParticleTupleID( python::module& );
+// void wrapReactionType( python::module& );
+// void wrapReactionID( python::module& );
+// void wrapFromEndfReactionNumber( python::module& );
+// void wrapToEndfReactionNumber( python::module& );
+// void wrapEmit( python::module& );
+// void wrapAbsorb( python::module& );
+// void wrapResolve( python::module& );
+
+/**
+ *  @brief Elementary python bindings
+ */
+PYBIND11_MODULE( elementary, module ) {
+
+  // wrapElementID( module );
+  // wrapFundamentalParticleID( module );
+  // wrapIsotopeID( module );
+  // wrapLevel( module );
+  // wrapNucleusID( module );
+  // wrapNuclideID( module );
+  // wrapParticleID( module );
+  // wrapParticlePairID( module );
+  // wrapParticleTupleID( module );
+  // wrapReactionType( module );
+  // wrapReactionID( module );
+  // wrapFromEndfReactionNumber( module );
+  // wrapToEndfReactionNumber( module );
+  // wrapEmit( module );
+  // wrapAbsorb( module );
+  // wrapResolve( module );
+}
